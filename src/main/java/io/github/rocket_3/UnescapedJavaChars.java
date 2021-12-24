7977ae53ea9a7ac67ab054dd125dc9448e5a4e33
rocket <gamingdas@gmail.com>
2021-12-24 12:35:48 +0300

    Fix package name Fix part of Qulice's warnings

--- src/main/java/io/github/rocket_3/UnescapedJavaChars.java
+++
@@ -1,9 +0,0 @@
-package io.github.rocket_3;
-
-public class UnescapedJavaChars extends CharsEnvelope {
-
-    public UnescapedJavaChars(final CharSequence chars) {
-        super(new UnescapeJavaChars().apply(chars));
-    }
-
-}
--- src/main/java/io/github/rocket_3/UnescapedYamlChars.java
+++
@@ -1,9 +0,0 @@
-package io.github.rocket_3;
-
-public class UnescapedYamlChars extends CharsEnvelope {
-
-    public UnescapedYamlChars(final CharSequence origin) {
-        super(new UnescapeYamlChars().apply(origin));
-    }
-
-}
